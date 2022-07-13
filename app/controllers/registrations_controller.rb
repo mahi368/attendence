8b2116a56a07c2843a8ad4c5291ff9507610a466
mahi368 <mahimayuvasoft@gmail.com>
2022-07-13 11:27:12 +0530

    remove debugger

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -2,7 +2,6 @@ class RegistrationsController < Devise::RegistrationsController
 	after_action :set_role , only: :create
 
 	def set_role
-		byebug
 		@role = Role.find(params[:user][:role_ids])
 		@user.roles << @role 
 	end
